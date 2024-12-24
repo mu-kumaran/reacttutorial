3106db08f73762c9ed43c7706f6a53ccf0055391
Manoj Kumar <mu.kumaran1706@gmail.com>
2024-12-24 17:40:37 +0530

    addition

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function Manoj()
   var mark1 = 35,mark2 = 25;
   var avg=67;
   var result = "fail";
-  var arr = [11,22,33,44,55,66];
+  var arr = [18,29,38,46,53,62];
   var sobj = {
     rollno : 1001,
     name : "Manoj",
