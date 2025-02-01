d682a17ca5f2cb18c369424e05d82e53c0e3a5b5
manoj <mu.kumaran1991@gmail.com>
2025-02-01 16:04:01 +0530

    new addition

--- src/App.js
+++ src/App.js
@@ -1,3 +1,86 @@
+// React - AXIOS
+
+/*import axios from "axios"
+import {useState} from "react"
+function App()
+{
+  const [city,setCity] = useState("")
+  const [output,setOutput] = useState(null)
+  const cityfun = (event)=>{
+    setCity(event.target.value)
+  }
+
+  const showreport = ()=>{
+    const apikey = "ac537bbe836e81e36aa7fffa32d483f3"
+    var apiurl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`
+    console.log(apiurl)
+    axios.get(apiurl).then(
+      (res) => {
+        console.log(res.data)
+        setOutput(res.data)
+      }
+    ).catch((err)=>{
+      console.log(err.response.data)
+      setOutput(err.response.data)
+    })
+  }
+  return(
+    <>
+    <h1>Axios</h1>
+    <h2>Weather report - using OpenWeathermap API</h2>
+    City: <input type="text" onChange={(e)=>cityfun(e)} placeholder="Enter city name" value={city}></input>
+    <br></br><br></br>
+    <input type="button" onClick={showreport} value="Get weather report"></input>
+    <br></br><br></br>
+    {output !== null && output.cod === 200 &&
+    <>
+      <h3>Weather Report:</h3>
+      <h4>{"Main: "+output.weather[0].main}</h4>
+      <h4>{"Description: "+output.weather[0].description}</h4>
+      <h4>{"Wind speed: "+output.wind.speed}</h4>
+    </> 
+    }
+    {output !== null && output.cod === "404" &&
+      <h3>{"Error: "+output.message}</h3>}
+    {output !== null && output.cod === "400" &&
+      <h3>{"Error: "+output.message}</h3>}
+    </>
+  )
+}
+export default App*/
+
+//React - HTTP and AXIOS
+/*import axios from "axios"
+import {useState} from "react"
+function App()
+{
+  const [city,setCity] = useState("")
+  const cityfun = (event)=>{
+    setCity(event.target.value)
+  }
+
+  const showreport = ()=>{
+    const apikey = "ac537bbe836e81e36aa7fffa32d483f3"
+    var apiurl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`
+    console.log(apiurl)
+    axios.get(apiurl).then(
+      (res) => {
+        console.log(res.data)
+      }
+    )
+  }
+  return(
+    <>
+    <h1>Axios</h1>
+    <h2>Weather report - using OpenWeathermap API</h2>
+    City: <input type="text" onChange={(e)=>cityfun(e)} placeholder="Enter city name" value={city}></input>
+    <br></br><br></br>
+    <input type="button" onClick={showreport} value="Get weather report"></input>
+    </>
+  )
+}
+export default App*/
+
 //React - class - Life cycle components
 
 //Unmount
